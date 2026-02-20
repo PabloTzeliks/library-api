58280db3673842953b63256e0b453ef714cc07a6
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-20 18:18:06 -0300

    fix: adjust user and password info of Database

--- src/main/java/com/centroweg/pablo/librarysystem/infra/persistence/repository/JpaUserRepository.java
+++ src/main/java/com/centroweg/pablo/librarysystem/infra/persistence/repository/JpaUserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface JpaUserRepository extends JpaRepository<Long, User> {
+public interface JpaUserRepository extends JpaRepository<User, Long> {
 
     User save(User user);
     Optional<User> findByEmail(String email);
