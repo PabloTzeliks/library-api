65f80a14875706b06015affa757fa4e18a38dfa0
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-22 13:04:54 -0300

    feat: authentication and registration of Users working

--- src/main/java/com/centroweg/pablo/librarysystem/infra/security/SecurityConfig.java
+++ src/main/java/com/centroweg/pablo/librarysystem/infra/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
