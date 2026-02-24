ace363efa5ba64f60f9233636597c5df1f22bcd8
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-24 15:56:46 -0300

    feat: new method for relational JpaRepository

--- src/main/java/com/centroweg/pablo/librarysystem/infra/persistence/repository/JpaUserBookRepository.java
+++ src/main/java/com/centroweg/pablo/librarysystem/infra/persistence/repository/JpaUserBookRepository.java
@@ -1,7 +1,12 @@
 package com.centroweg.pablo.librarysystem.infra.persistence.repository;
 
+import com.centroweg.pablo.librarysystem.domain.User;
 import com.centroweg.pablo.librarysystem.domain.UserBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JpaUserBookRepository extends JpaRepository<UserBook, Long> {
+
+    List<UserBook> findByUser(User user);
 }
