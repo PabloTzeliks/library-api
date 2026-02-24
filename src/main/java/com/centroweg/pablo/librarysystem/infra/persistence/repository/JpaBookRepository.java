66941ea190d759d289e26b3ae0cc4c0c061bc185
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-24 15:51:46 -0300

    feat: implement initial BookService

--- src/main/java/com/centroweg/pablo/librarysystem/infra/persistence/repository/JpaBookRepository.java
+++ src/main/java/com/centroweg/pablo/librarysystem/infra/persistence/repository/JpaBookRepository.java
@@ -3,5 +3,9 @@ package com.centroweg.pablo.librarysystem.infra.persistence.repository;
 import com.centroweg.pablo.librarysystem.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaBookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByIsbn(String isbn);
 }
