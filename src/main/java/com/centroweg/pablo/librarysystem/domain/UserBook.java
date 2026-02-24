c8518abea55e0b6ad75b294b1ebd1da2e4088a56
PABLO RUAN TZELIKS <pablo_tzeliks@estudante.sesisenai.org.br>
2026-02-24 14:40:12 -0300

    feat: implement 'UserBook' Entity

--- src/main/java/com/centroweg/pablo/librarysystem/domain/UserBook.java
+++ src/main/java/com/centroweg/pablo/librarysystem/domain/UserBook.java
@@ -13,5 +13,25 @@ import java.time.LocalDateTime;
 @Table(name = "user_book")
 public class UserBook {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column(name = "status", nullable = false)
+    private BookStatus status;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
