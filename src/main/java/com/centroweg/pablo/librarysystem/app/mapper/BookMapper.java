b00f0ba42bb243a175ee5a56691523a083efb851
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-24 15:25:33 -0300

    feat: implement initial Mapper for Book

--- src/main/java/com/centroweg/pablo/librarysystem/app/mapper/BookMapper.java
+++ src/main/java/com/centroweg/pablo/librarysystem/app/mapper/BookMapper.java
@@ -1,8 +1,18 @@
 package com.centroweg.pablo.librarysystem.app.mapper;
 
+import com.centroweg.pablo.librarysystem.app.dto.BookRequest;
 import com.centroweg.pablo.librarysystem.domain.Book;
 
 public class BookMapper {
 
-    public Book toEntity()
+    public Book toDomain(BookRequest request, Long id) {
+
+        return new Book(
+                id,
+                request.isbn(),
+                request.title(),
+                request.authors(),
+                request.thumbnailUrl()
+        );
+    }
 }
